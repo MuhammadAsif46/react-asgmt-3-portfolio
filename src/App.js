4bea37e152a3a5a52ab442834b60890c521316aa
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-27 12:02:34 +0500

    added navbar in portfolio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { EmojiSmile } from "react-bootstrap-icons";
+import { BiSmile, BiGlobe, BiLogoLinkedin} from 'react-icons/bi';
+import { BsList } from "react-icons/bs";
 import "./App.css";
+
+/* <div className="heading">
+        <h1>Personal <span className="yellow">Portfolio</span> Web</h1>
+      </div> */
 
 function App() {
   return (
-    <>
-      <div className="heading">
-        <h1>Personal <span className="yellow">Portfolio</span> Web</h1>
+    <div className="main">
+      <div className="box-1">
+        <nav>
+          <div className="port-nav">
+            <div><BiSmile className='yellow'/>ASIF</div>
+            <div className='pages'>
+                <a className="page" href="">Home</a>
+                <a className="page" href="">Portfolio</a>
+                <a className="page" href="">contact</a>
+                <a className="page" href="">About</a>
+            </div>
+            <div className='icons'>
+              <ul>
+                <li> <BiGlobe className='icon'/> <BiLogoLinkedin className='icon'/> <BsList className='icon'/> </li>
+              </ul>
+            </div>
+          </div>
+        </nav>  
       </div>
-      <div className="main">
-        <div className="box-1">
+      <div className="box-2"></div>
           
-        </div>
-        <div className="box-2"></div>
-          
-      </div>
-    </>
+    </div>
   );
 }
 
