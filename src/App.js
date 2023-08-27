be28d04a5f0aaf40b7fc9b818f6b0260eab1ef5c
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-27 10:07:31 +0500

    create heading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import { EmojiSmile } from "react-bootstrap-icons";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div>
-        
-        <h1></h1>
+    <>
+      <div className="heading">
+        <h1>Personal <span className="yellow">Portfolio</span> Web</h1>
       </div>
-    </div>
+      <div className="main">
+        <div className="box-1">
+          
+        </div>
+        <div className="box-2"></div>
+          
+      </div>
+    </>
   );
 }
 
